043e01d71d4b8aa59faab58a05478280b9ab8e5c
AnshulHedau <anshulhedau01@gmail.com>
2017-04-23 00:02:13 +0530

    Added Animation with bug fixed

--- app/src/main/java/anshul/software_project/CustomList.java
+++ app/src/main/java/anshul/software_project/CustomList.java
@@ -2,7 +2,6 @@ package anshul.software_project;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
--- app/src/main/java/anshul/software_project/HomeActivity.java
+++ app/src/main/java/anshul/software_project/HomeActivity.java
@@ -2,8 +2,8 @@ package anshul.software_project;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
